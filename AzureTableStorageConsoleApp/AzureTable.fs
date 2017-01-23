c149bb634380a920fc7340e915b263c94375348e
James Oakley <JOakley@compasshealth.local>
2017-01-23 14:29:54 +1300

    init

---
+++ AzureTableStorageConsoleApp/AzureTable.fs
@@ -0,0 +1,32 @@
+﻿namespace AzureTableClient
+
+open Microsoft.Azure
+open Microsoft.WindowsAzure.Storage
+open Microsoft.WindowsAzure.Storage.Table
+
+module AzureTable = 
+    open System
+
+    type LocationEntity(param1) =
+        inherit TableEntity(DateTime.Today.ToString("yyyy-MM-dd"), (Guid.NewGuid ()).ToString ())
+        member this.Param1 = param1
+        
+    let init:CloudTable = 
+
+        let csa:CloudStorageAccount = CloudStorageAccount.Parse (CloudConfigurationManager.GetSetting "StorageConnectionString")
+        let ctc = csa.CreateCloudTableClient ()
+        let location = ctc.GetTableReference "location"
+
+        //location.Delete () |> ignore
+        
+        let created:bool = location.CreateIfNotExists () 
+        
+        let loc1 = new LocationEntity("some param")
+        
+        let insertOperation:TableOperation = TableOperation.Insert(loc1)        
+        let tableResult:TableResult = location.Execute(insertOperation) 
+        
+       
+        location
+        //()
+        //(fun () -> ())
--- AzureTableStorageConsoleApp/Program.fs
+++ AzureTableStorageConsoleApp/Program.fs
@@ -1,7 +1,17 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+
+
+
+open AzureTableClient.AzureTable
+
+
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
+    
+    let x = init
+    
+    System.Console.ReadKey() |> ignore
     0 // return an integer exit code
